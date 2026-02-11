2dd5cb1ef5a5a49632a5c73f6f44045c48d7d455
Mohamed Atef <mo5460642@gmail.com>
2026-02-11 04:45:41 +0200

    Updating youtube channels's ID

--- app/services/config.py
+++ app/services/config.py
@@ -1,4 +1,6 @@
 YOUTUBE_CHANNELS = [
     # "UCn8ujwUInbJkBhffxqAPBVQ", # Dave Ebbelaar
-    "UCawZsQWqfGSbCI5yjkdVkTA", # Matthew Berman
+    # "UCawZsQWqfGSbCI5yjkdVkTA", # Matthew Berman
+    # "UChc0Yw9NkSb8Thy53MsM3gw", # المواطن سعيد
+    "UC3WziBdrb8ztqlt0n4k9dNw", #Summary
 ]
